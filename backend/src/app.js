8444733f9aa04311a72257b0e84e2a50cd7113b6
Nimesha Gunawardana <nimeshae@gmail.com>
2021-03-26 12:30:41 +0530

    app.js

---
+++ backend/src/app.js
@@ -0,0 +1,37 @@
+import express from "express";
+const app = express();
+
+import bodyParser from "body-parser";
+import Mongoose from "mongoose";
+
+import { MONGO_URL } from "./config/index.js";
+
+//middleware
+app.use(bodyParser.json());
+
+// // Routers File Paths
+import galleryUploder from "./api/router/photo.js";
+
+// // Router's URl paths
+
+app.use("/", galleryUploder);
+
+// DB connection
+Mongoose.connect(
+  MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  (error) => {
+    if (!error) {
+      console.log("Success");
+    } else {
+      console.log(error);
+    }
+  }
+);
+
+export default app;
