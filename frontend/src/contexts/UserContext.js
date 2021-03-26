4c60736749f605ad5d0e8a3bc1a4411ec684aae1
Nimesha Gunawardana <nimeshae@gmail.com>
2021-03-26 15:44:23 +0530

    user context change

--- frontend/src/contexts/UserContext.js
+++ frontend/src/contexts/UserContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react';
 export const UserContext = createContext();
 
 const UserContextProvider = (props) => {
-    const [user, setUser] = useState([]);
+    const [user, setUser] = useState();
 
     const addUser = (id) => {
         setUser({ 'user_id': id });
